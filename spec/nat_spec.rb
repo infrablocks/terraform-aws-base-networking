835c8e1f55ec5de28e767149f52debf513fc4f46
Toby Clemson <tobyclemson@gmail.com>
2017-02-16 00:59:57 +0000

    Output NAT public IP.

--- spec/nat_spec.rb
+++ spec/nat_spec.rb
@@ -21,4 +21,13 @@ describe 'NAT' do
   it 'resides in the first public subnet' do
     expect(subject.subnet_id).to(eq(first_public_subnet.id))
   end
+
+  it 'associates an EIP and exposes as an output' do
+    public_ip = nat_public_ip_output
+    expect(subject.nat_gateway_addresses.map(&:public_ip)).to(include(public_ip))
+  end
+
+  def nat_public_ip_output
+    Terraform.output(name: 'nat_public_ip')
+  end
 end
--- src/outputs.tf
+++ src/outputs.tf
@@ -33,3 +33,7 @@ output "private_subnet_cidr_blocks" {
 output "bastion_public_ip" {
   value = "${aws_eip.bastion.public_ip}"
 }
+
+output "nat_public_ip" {
+  value = "${aws_eip.nat.public_ip}"
+}
