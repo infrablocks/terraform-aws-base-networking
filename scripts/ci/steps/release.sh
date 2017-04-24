34966e898f8314d0bcf7273595b473d49aff2a33
Toby Clemson <tobyclemson@gmail.com>
2017-04-25 00:00:23 +0100

    Add rake task and CI script to increment tag on successful build.

---
+++ scripts/ci/steps/release.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+[ -n "$DEBUG" ] && set -x
+set -e
+set -o pipefail
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROJECT_DIR="$( cd "$SCRIPT_DIR/../../.." && pwd )"
+
+cd "$PROJECT_DIR"
+
+openssl aes-256-cbc \
+    -d \
+    -in ./.circleci/gpg.private.enc -k "${ENCRYPTION_PASSPHRASE}" | gpg --import -
+git crypt unlock
+
+./go release:tag
