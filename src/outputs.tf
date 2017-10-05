f8e216c7794ce387f7905fec3a34a4cbd007ffd4
Toby Clemson <tobyclemson@gmail.com>
2017-10-05 11:59:12 +0100

    [TECH] Remove bastion now that another module handles bastion creation.
    Authors: @tobyclemson

--- src/outputs.tf
+++ src/outputs.tf
@@ -38,14 +38,6 @@ output "private_route_table_id" {
   value = "${aws_route_table.private.id}"
 }
 
-output "bastion_public_ip" {
-  value = "${aws_eip.bastion.public_ip}"
-}
-
 output "nat_public_ip" {
   value = "${aws_eip.nat.public_ip}"
-}
-
-output "open_to_bastion_security_group_id" {
-  value = "${aws_security_group.open_to_bastion.id}"
 }
--- src/variables.tf
+++ src/variables.tf
@@ -8,13 +8,6 @@ variable "dependencies" {
   default = ""
 }
 
-variable "bastion_ami" {}
-variable "bastion_instance_type" {
-  default = "t2.nano"
-}
-variable "bastion_ssh_public_key_path" {}
-variable "bastion_ssh_allow_cidrs" {}
-
 variable "domain_name" {}
 variable "public_zone_id" {}
 variable "private_zone_id" {}
