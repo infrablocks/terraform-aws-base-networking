9a8ce5a4c25725e404310ff7d02047d8313bf25b
Toby Clemson <tobyclemson@gmail.com>
2017-12-29 00:41:46 +0000

    Remove redundant information from variable descriptions.

--- variables.tf
+++ variables.tf
@@ -1,18 +1,18 @@
 variable "vpc_cidr" {
-  description = "The CIDR to use for the VPC. Required."
+  description = "The CIDR to use for the VPC."
 }
 variable "region" {
-  description = "The region into which to deploy the VPC. Required."
+  description = "The region into which to deploy the VPC."
 }
 variable "availability_zones" {
-  description = "The availability zones for which to add subnets. Required."
+  description = "The availability zones for which to add subnets."
 }
 
 variable "component" {
-  description = "The component this network will contain. Required."
+  description = "The component this network will contain."
 }
 variable "deployment_identifier" {
-  description = "An identifier for this instantiation. Required."
+  description = "An identifier for this instantiation."
 }
 variable "dependencies" {
   description = "A comma separated list of components depended on my this component."
@@ -20,23 +20,23 @@ variable "dependencies" {
 }
 
 variable "public_subnets_offset" {
-  description = "The number of /24s to offset the public subnets in the VPC CIDR. Defaults to 0."
+  description = "The number of /24s to offset the public subnets in the VPC CIDR."
   default = 0
 }
 variable "private_subnets_offset" {
-  description = "The number of /24s to offset the private subnets in the VPC CIDR. Defaults to 0."
+  description = "The number of /24s to offset the private subnets in the VPC CIDR."
   default = 0
 }
 
 variable "private_zone_id" {
-  description = "The ID of the private Route 53 zone. Required."
+  description = "The ID of the private Route 53 zone."
 }
 
 variable "include_lifecycle_events" {
-  description = "Whether or not to notify via S3 of a created VPC. Defaults to 'yes'."
+  description = "Whether or not to notify via S3 of a created VPC."
   default = "yes"
 }
 variable "infrastructure_events_bucket" {
-  description = "S3 bucket in which to put VPC creation events. Required if `include_lifecycle_events` is 'yes'."
+  description = "S3 bucket in which to put VPC creation events. Required when `include_lifecycle_events` is 'yes'."
   default = ""
 }
