ccf4af1355cfd9722121e6a5f2bc2791113a9bd7
Vlad Romanenko <vlad.romanenko@hotmail.com>
2019-06-03 11:33:04 +0100

    Make it possible to run destroy multiple times

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "vpc_id" {
   description = "The ID of the created VPC."
-  value = "${aws_vpc.base.id}"
+  value = "${element(concat(aws_vpc.base.*.id, list("")), 0)}"
 }
 
 output "vpc_cidr" {
   description = "The CIDR of the created VPC."
-  value = "${aws_vpc.base.cidr_block}"
+  value = "${element(concat(aws_vpc.base.*.cidr_block, list("")), 0)}"
 }
 
 output "availability_zones" {
@@ -30,7 +30,7 @@ output "public_subnet_cidr_blocks" {
 
 output "public_route_table_id" {
   description = "The ID of the public route table."
-  value = "${aws_route_table.public.id}"
+  value = "${element(concat(aws_route_table.public.*.id, list("")), 0)}"
 }
 
 output "private_subnet_ids" {
@@ -45,7 +45,7 @@ output "private_subnet_cidr_blocks" {
 
 output "private_route_table_id" {
   description = "The ID of the private route table."
-  value = "${aws_route_table.private.id}"
+  value = "${element(concat(aws_route_table.private.*.id, list("")), 0)}"
 }
 
 output "nat_public_ip" {
