333702779c47078015199c4c6b445849b9ff98ed
John Cowie Del Corral <john@go-atomic.io>
2025-10-10 10:21:55 +0200

    Revert "Merge pull request #141 from infrablocks/support-az-updates"
    This reverts commit 0e05f060a9c949db1de90a3a8f9fce7ea4593d03, reversing
    changes made to e0396ae8f6e31c36845b6c03a21f6d05182d4013.

--- private_subnets.tf
+++ private_subnets.tf
@@ -1,12 +1,11 @@
 resource "aws_subnet" "private" {
-  for_each = toset(var.availability_zones)
-
   vpc_id = aws_vpc.base.id
-  cidr_block = cidrsubnet(var.vpc_cidr, 8, index(var.availability_zones, each.value) + length(var.availability_zones) + local.private_subnets_offset)
-  availability_zone = each.value
+  count = length(var.availability_zones)
+  cidr_block = cidrsubnet(var.vpc_cidr, 8, count.index + length(var.availability_zones) + local.private_subnets_offset)
+  availability_zone = element(var.availability_zones, count.index)
 
   tags = {
-    Name = "private-subnet-${var.component}-${var.deployment_identifier}-${each.value}"
+    Name = "private-subnet-${var.component}-${var.deployment_identifier}-${element(var.availability_zones, count.index)}"
     Component = var.component
     DeploymentIdentifier = var.deployment_identifier
     Tier = "private"
@@ -14,12 +13,11 @@ resource "aws_subnet" "private" {
 }
 
 resource "aws_route_table" "private" {
-  for_each = toset(var.availability_zones)
-
   vpc_id = aws_vpc.base.id
+  count = length(var.availability_zones)
 
   tags = {
-    Name = "private-routetable-${var.component}-${var.deployment_identifier}-${each.value}"
+    Name = "private-routetable-${var.component}-${var.deployment_identifier}-${element(var.availability_zones, count.index)}"
     Component = var.component
     DeploymentIdentifier = var.deployment_identifier
     Tier = "private"
@@ -27,16 +25,14 @@ resource "aws_route_table" "private" {
 }
 
 resource "aws_route" "private_internet" {
-  for_each = local.include_nat_gateways == "yes" ? toset(var.availability_zones) : toset([])
-
-  route_table_id = aws_route_table.private[each.value].id
-  nat_gateway_id = aws_nat_gateway.base[each.value].id
+  count = local.include_nat_gateways == "yes" ? length(var.availability_zones) : 0
+  route_table_id = element(aws_route_table.private.*.id, count.index)
+  nat_gateway_id = element(aws_nat_gateway.base.*.id, count.index)
   destination_cidr_block = "0.0.0.0/0"
 }
 
 resource "aws_route_table_association" "private" {
-  for_each = toset(var.availability_zones)
-
-  subnet_id = aws_subnet.private[each.value].id
-  route_table_id = aws_route_table.private[each.value].id
+  count = length(var.availability_zones)
+  subnet_id = element(aws_subnet.private.*.id, count.index)
+  route_table_id = element(aws_route_table.private.*.id, count.index)
 }
