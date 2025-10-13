3091fae85a80b434bdc6411f5bca3aca0a6e464c
John Cowie Del Corral <john@go-atomic.io>
2025-10-13 10:24:12 +0200

    Auto-fix some formatting issues

--- spec/integration/availability_zone_addition_spec.rb
+++ spec/integration/availability_zone_addition_spec.rb
@@ -20,10 +20,10 @@ describe 'availability zone addition' do
   describe 'adding a new availability zone' do
     let(:test_dir) { @test_dir }
 
-    after(:each) do
+    after do
       # Cleanup: destroy all resources created during the test
       if @test_dir && Dir.exist?(@test_dir)
-        terraform_exec = from_root_directory("vendor/terraform/bin/terraform")
+        terraform_exec = from_root_directory('vendor/terraform/bin/terraform')
         Dir.chdir(@test_dir) do
           system("#{terraform_exec} destroy -auto-approve")
         end
@@ -35,9 +35,12 @@ describe 'availability zone addition' do
       initial_state = apply_and_get_state(initial_availability_zones)
 
       # Get the initial subnet IDs
-      initial_public_subnet_ids = get_resource_ids(initial_state, 'aws_subnet', 'public')
-      initial_private_subnet_ids = get_resource_ids(initial_state, 'aws_subnet', 'private')
-      initial_nat_gateway_ids = get_resource_ids(initial_state, 'aws_nat_gateway', 'base')
+      initial_public_subnet_ids = get_resource_ids(initial_state, 'aws_subnet',
+                                                   'public')
+      initial_private_subnet_ids = get_resource_ids(initial_state,
+                                                    'aws_subnet', 'private')
+      initial_nat_gateway_ids = get_resource_ids(initial_state,
+                                                 'aws_nat_gateway', 'base')
       initial_eip_ids = get_resource_ids(initial_state, 'aws_eip', 'nat')
 
       # Step 2: Plan with additional availability zone
@@ -60,12 +63,15 @@ describe 'availability zone addition' do
 
       # Assert no existing resources are being destroyed
       expect(destroyed_resources).to be_empty,
-        "Expected no resources to be destroyed, but found: #{destroyed_resources.map { |r| "#{r['type']}.#{r['name']}" }.join(', ')}"
+                                     "Expected no resources to be destroyed, but found: #{destroyed_resources.map do |r|
+                                                                                            "#{r['type']}.#{r['name']}"
+                                                                                          end.join(', ')}"
 
       # Check that only new resources are being created
       created_resources = resource_changes.select do |change|
         change['change']['actions'].include?('create') &&
-          %w[aws_subnet aws_route_table aws_route_table_association aws_route aws_nat_gateway aws_eip].include?(change['type'])
+          %w[aws_subnet aws_route_table aws_route_table_association aws_route
+             aws_nat_gateway aws_eip].include?(change['type'])
       end
 
       # We expect exactly 1 new public subnet, 1 new private subnet,
@@ -81,12 +87,12 @@ describe 'availability zone addition' do
       }
 
       actual_new_resources = created_resources.group_by { |r| r['type'] }
-        .transform_values(&:count)
+                                              .transform_values(&:count)
 
       expected_new_resources.each do |resource_type, expected_count|
         actual_count = actual_new_resources[resource_type] || 0
         expect(actual_count).to eq(expected_count),
-          "Expected #{expected_count} new #{resource_type} resources, but found #{actual_count}"
+                                "Expected #{expected_count} new #{resource_type} resources, but found #{actual_count}"
       end
     end
   end
@@ -94,25 +100,26 @@ describe 'availability zone addition' do
   private
 
   def apply_and_get_state(availability_zones)
-    terraform_exec = from_root_directory("vendor/terraform/bin/terraform")
+    terraform_exec = from_root_directory('vendor/terraform/bin/terraform')
     # Create a temporary directory for this test run
     @test_dir = "spec/integration/test_runs/#{Time.now.to_i}"
     FileUtils.mkdir_p(@test_dir)
 
     # Write the terraform configuration
-    File.write("#{@test_dir}/main.tf", generate_terraform_config(availability_zones))
+    File.write("#{@test_dir}/main.tf",
+               generate_terraform_config(availability_zones))
 
     # Initialize and apply
     Dir.chdir(@test_dir) do
       init_result = system("#{terraform_exec} init")
-      raise "Terraform init failed" unless init_result
+      raise 'Terraform init failed' unless init_result
 
       apply_result = system("#{terraform_exec} apply -auto-approve")
-      raise "Terraform apply failed" unless apply_result
+      raise 'Terraform apply failed' unless apply_result
 
       # Get the state - properly write to file
       state_json = `#{terraform_exec} show -json`
-      File.write("initialstate.json", state_json)
+      File.write('initialstate.json', state_json)
       JSON.parse(state_json)
     end
   end
@@ -120,9 +127,10 @@ describe 'availability zone addition' do
   def plan_with_azs(availability_zones)
     # Update the configuration with new AZs
     test_dir = Dir.glob('spec/integration/test_runs/*').last
-    File.write("#{test_dir}/main.tf", generate_terraform_config(availability_zones))
+    File.write("#{test_dir}/main.tf",
+               generate_terraform_config(availability_zones))
 
-    terraform_exec = from_root_directory("vendor/terraform/bin/terraform")
+    terraform_exec = from_root_directory('vendor/terraform/bin/terraform')
 
     # Run plan and capture output
     Dir.chdir(test_dir) do
@@ -148,7 +156,7 @@ describe 'availability zone addition' do
       }
 
       module "base_networking" {
-        source = "#{from_root_directory("")}"
+        source = "#{from_root_directory('')}"
 
         vpc_cidr                         = "#{vpc_cidr}"
         region                           = "#{region}"
@@ -161,22 +169,22 @@ describe 'availability zone addition' do
   end
 
   def from_root_directory(dir)
-    return "../../../../#{dir}"
+    "../../../../#{dir}"
   end
 
   def get_resource_ids(state, resource_type, resource_name)
     resources = state['values']['root_module']['child_modules']
-      &.first['resources'] || []
+                &.first&.[]('resources') || []
 
     resources.select do |r|
       r['type'] == resource_type && r['name'] == resource_name
-    end.map do |r|
+    end.to_h do |r|
       # For for_each resources, use the index key (AZ name) as the key
       if r['index'].is_a?(String)
         [r['index'], r['values']['id']]
       else
         [r['index'].to_s, r['values']['id']]
       end
-    end.to_h
+    end
   end
 end
